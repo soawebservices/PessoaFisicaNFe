ad2ff5d7de438ea3bd861e2ea7ef1d9fc7ab953c
Angelo Bestetti <angelobestetti@gmail.com>
2015-06-04 15:47:23 -0300

    Update ConsultaPessoaFisicaNFe.php

--- ConsultaPessoaFisicaNFe.php
+++ ConsultaPessoaFisicaNFe.php
@@ -6,9 +6,10 @@ $credenciais        = new Credenciais();
 $credenciais->Email = 'seu email aqui';
 $credenciais->Senha = 'sua senha aqui';
 
-$pfnfe                = new PessoaFisicaNFe();
-$pfnfe->Credenciais   = $credenciais;
-$pfnfe->Documento     = 'documento a ser consultado';
+$pfnfe                 = new PessoaFisicaNFe();
+$pfnfe->Credenciais    = $credenciais;
+$pfnfe->Documento      = 'documento a ser consultado';
+$pfnfe->DataNascimento = 'Data de Nascimento do Consultado';
 
 $cdc = new CDC();
 $pfnfe = $cdc->getPessoaFisicaNFe($pfnfe);
@@ -16,14 +17,19 @@ $pfnfe = $cdc->getPessoaFisicaNFe($pfnfe);
 echo "\n\n-----------------------   INFORMACOES GERAIS   -----------------------\n\n\n";
 
 # PRINT PROPRIEDADES DO OBJETO
-echo 'Documento:        ' . $pfnfe->Documento . "\n";
-echo 'Nome:             ' . $pfnfe->Nome . "\n";
-echo 'SituacaoRFB:      ' . $pfnfe->SituacaoRFB . "\n";
-echo 'DataConsultaRFB:  ' . date('d/m/Y H:i:s', $pfnfe->DataConsultaRFB) . "\n";
-echo 'ProtocoloRFB:     ' . $pfnfe->ProtocoloRFB . "\n";
-echo 'DigitoVerificador:' . $pfnfe->DigitoVerificador . "\n";
-echo 'Mensagem:         ' . $pfnfe->Mensagem . "\n";
-echo 'Status:           ' . $pfnfe->Status . "\n";
+echo 'Documento:                  ' . $pfnfe->Documento . "\n";
+echo 'Nome:                       ' . $pfnfe->Nome . "\n";
+echo 'Data Nascimento:            ' . date('d/m/Y H:i:s', $pfnfe->DataNascimento) . "\n";
+echo 'Data Inscricao:             ' . date('d/m/Y H:i:s', $pfnfe->DataInscrcao) . "\n";
+echo 'Ano Obito:                  ' . $pfnfe->AnoObito . "\n";
+echo 'Mensagem Obito:             ' . $pfnfe->MensagemObito . "\n";
+echo 'Codigo Situacao Cadastral:  ' . $pfnfe->CodigoSituacaoCadastral . "\n";
+echo 'SituacaoRFB:                ' . $pfnfe->SituacaoRFB . "\n";
+echo 'DataConsultaRFB:            ' . date('d/m/Y H:i:s', $pfnfe->DataConsultaRFB) . "\n";
+echo 'ProtocoloRFB:               ' . $pfnfe->ProtocoloRFB . "\n";
+echo 'DigitoVerificador:          ' . $pfnfe->DigitoVerificador . "\n";
+echo 'Mensagem:                   ' . $pfnfe->Mensagem . "\n";
+echo 'Status:                     ' . $pfnfe->Status . "\n";
 
 echo "\n----------------------------------------------------------------------\n\n\n";
 
